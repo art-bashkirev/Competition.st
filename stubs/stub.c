895c4575c09a0f1bb68eb29472637bfa42d457d3
Artem Bashkirev <134786179+art-bashkirev@users.noreply.github.com>
2023-12-02 09:49:58 +0300

    Create stub.c

---
+++ stubs/stub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define mod 1000000007
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
